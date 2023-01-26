f32916d6a7b5bd1a3b17674ba240edfabe53b10b
diegotimao <diegotimao1104@gmail.com>
2023-01-26 17:00:53 -0500

    feat: Implementando a função is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+
+    if word[high_index] != word[low_index]:
+        return False
+
+    if high_index == 0:
+        return True
+    else:
+        return is_palindrome_recursive(word, (low_index + 1), (high_index - 1))
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -3,7 +3,7 @@ from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    input_message = 'Hello'
+    input_message = 'Helloa'
 
     with pytest.raises(TypeError, match="tipo inválido para key"):
         encrypt_message('Hello', '2')
@@ -11,7 +11,7 @@ def test_encrypt_message():
     with pytest.raises(TypeError, match="tipo inválido para message"):
         encrypt_message(2, 2)
 
-    assert encrypt_message(input_message, -2) == 'olleH'
-    assert encrypt_message(input_message, 2) == 'oll_eH'
-    assert encrypt_message(input_message, 1) == 'H_olle'
-    assert encrypt_message(input_message, 3) == 'leH_ol'
+    assert encrypt_message(input_message, -2) == 'aolleH'
+    assert encrypt_message(input_message, 2) == 'aoll_eH'
+    assert encrypt_message(input_message, 1) == 'H_aolle'
+    assert encrypt_message(input_message, 3) == 'leH_aol'
