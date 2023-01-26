b6213bd6c9bd19e8e859b4ddaeff29d6893d9eb7
diegotimao <diegotimao1104@gmail.com>
2023-01-25 21:11:00 -0500

    test: Testando se a função levanta as exeções ao passar parâmentros inválidos

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    input_message = 'Hello'
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message('Hello', '2')
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(2, 2)
