785e4eaf862f091a1525fbe38ff2e5c4c41a58af
diegotimao <diegotimao1104@gmail.com>
2023-01-29 02:06:56 -0500

    feat: Criando função auxiliar para que retorna uma lista de com os index de cada letra no alfabeto

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,22 @@
+alfa = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'L', 'N', 'M', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Z']
+
+
+def list_numbers(list):
+    list_index = []
+
+    for index in list:
+
+        for search_index in alfa:
+            if search_index == index.upper():
+                list_index.append(alfa.index(search_index))
+    
+    return list_index
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    
+    first_str = list_numbers(first_string)
+    second_str = list_numbers(second_string)
+    
+    print(first_str)
+    print(second_str)
+
