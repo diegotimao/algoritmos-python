10fb1b89e4c07991ff801aa9b2f348dea4ef15ca
diegotimao <diegotimao1104@gmail.com>
2023-01-30 21:29:54 -0500

    feat: Implementando a função is_palindrome_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,13 @@
+def reverse_string(word):
+    if str(word) == str(word)[::-1]:
+        return True
+    else:
+        return False
+
+
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+
+    word_invert = reverse_string(word)
+    return word_invert
