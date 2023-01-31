800538379a9dd4d799331ae937af73514ebe466b
diegotimao <diegotimao1104@gmail.com>
2023-01-30 19:57:22 -0500

    feat: Implementando a função find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,14 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    numbers = sorted(nums)
+
+    if len(nums) < 2:
+        return False
+
+    for item in range(len(numbers) - 1):
+        type_item = isinstance(numbers[item], int)
+
+        if not type_item or numbers[item] < 0:
+            return False
+        if numbers[item] == numbers[item + 1]:
+            return numbers[item]
+    return False
